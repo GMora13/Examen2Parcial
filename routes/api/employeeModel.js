08c2d4cec37be29ebe75e7ae86de1fcb81ebecd0
GMora13 <jose.mora_13@outlook.com>
2019-03-12 19:37:58 -0600

    Employee by tag

--- routes/api/employeeModel.js
+++ routes/api/employeeModel.js
@@ -3,6 +3,7 @@ var ObjectID = require('mongodb').ObjectID;
 function employeeModel(db){
   var lib = {};
   var empColl = db.collection('emps');
+  
   lib.getEmployees = (handler)=>{
     // implementar
     // obtener todos los documentos
@@ -19,47 +20,72 @@ function employeeModel(db){
   }
 
   lib.getEmployeesById = (id, handler) => {
-    // implementar
-    // Obtener un Documento solo mostrar
-    // email, phone, name y age
-    return handler(new Error("No Implementado"), null);
+    obt.findOne({ "_id": new ObjectId(thingId)}, (err, doc)=>{
+      if(err){
+        handler(err, null);
+      }else{
+        handler(null, doc);
+      }
+    });
+    // return handler(new Error("No Implementado"), null);
   }
 
   lib.getEmployeesByCompany = (company, handler) => {
-    // implementar
-    // solo mostrar name, email, company
-    return handler(new Error("No Implementado"), null);
+    obt.findOne({ "company": new ObjectId(company)}, (err, doc)=>{
+      if(err){
+        handler(err, null);
+      }else{
+        handler(null, doc);
+      }
+    });
+   // return handler(new Error("No Implementado"), null);
   }
 
   lib.getEmployeesByAgeRange = (ageLowLimit, ageHighLimit, handler) => {
-    //implementar
-    // Mostrar todos los documento incluyendo los extremos
-    // que esten entre las edades indicadas por los parametros
-    // ageLowLimit y ageHighLimit
-    // solo mostrar name, age, email
-    return handler(new Error("No Implementado"), null);
+    obt.findOne({ "Age": new ObjectId(ageHighLimit)}, (err, doc)=>{
+      if(err){
+        handler(err, null);
+      }else{
+        handler(null, doc);
+      }
+    });
+   // return handler(new Error("No Implementado"), null);
   }
 
   lib.getEmployeesByTag = (tag, handler) => {
-    //implementar
-    // obtener todos los documentos que contenga 
-    // al menos una vez el tag dentro del arreglo
-    // tags
-    // mostrar solo name, email, tags
-    return handler(new Error("No Implementado"), null);
+    obt.findOne({ "tag": new ObjectId(tag)}, (err, doc)=>{
+      if(err){
+        handler(err, null);
+      }else{
+        handler(null, doc);
+      }
+    });
+   // return handler(new Error("No Implementado"), null);
   }
 
   lib.addEmployeeATag = ( tag, id, handler) => {
-    //Implementar
-    //Se requiere agregar a un documento un nuevo tag
-    // $push
-    return handler(new Error("No Implementado"), null);
+    var curatedTags = Array.isArray(tags)? tags: [tags];
+    var updateObject = { "$set": { "tags": curatedTags}};
+    obt.updateOne({"_id": ObjectId(id)}, updateObject, (err, rsult)=>{
+        if(err){
+          handler(err, null);
+        }else{
+          handler(null, rsult.result);
+        }
+    } );
+    //return handler(new Error("No Implementado"), null);
   }
 
   lib.removeEmployee = (id, handler) => {
-    //Implementar
-    //Se requiere eliminar un documento de la colección
-    return handler(new Error("No Implementado"), null);
+    obt.deleteOne({"_id": ObjectId(Id)}, (err, rslt)=>{
+      if(err){
+        console.log(err);
+        handler(err, null);
+      } else {
+        handler(null, rslt.result);
+      }
+    });
+    //return handler(new Error("No Implementado"), null);
   }
 
   lib.increaseAgeToAll = (ageDelta, handler) => {
